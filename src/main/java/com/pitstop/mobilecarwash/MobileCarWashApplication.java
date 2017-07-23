54b5b12806d13cfa7ab99913d59a187bb93bab61
Emmie24 <emmanuelmadupo@gmail.com>
2017-07-23 09:43:47 +0200

    changed port number on back-end

--- src/main/java/com/pitstop/mobilecarwash/MobileCarWashApplication.java
+++ src/main/java/com/pitstop/mobilecarwash/MobileCarWashApplication.java
@@ -80,19 +80,39 @@ public class MobileCarWashApplication {
 
 	@Bean
 	@Profile("QA")
-	public CommandLineRunner testingDemoQA(UserRepository userRepository, UserService userService, RoleRepository roleRepository, RoleService roleService) {
+	public CommandLineRunner testingDemoQA(UserRepository userRepository, UserService userService, RoleRepository roleRepository, RoleService roleService, ComplexRepository complexRepository, ComplexService complexService, WashTypeRepository washTypeRepository, WashTypeService washTypeService, WashTimeRepository washTimeRepository, WashTimeService washTimeService) {
 		return (args) -> {
-			/*Role role = new Role("user");
+			Role role = new Role("user");
 			Role roleAdmin = new Role("admin");
 			roleService.addRole(role);
-			roleService.addRole(roleAdmin);*/
+			roleService.addRole(roleAdmin);
 
+			//// TODO: 2017/06/30 add default complex
+			Complex complex = new Complex("First Complex","46 Geranium Street, Birch Acres, Kempton Park",new Date(),new Date());
+			complexService.addComplex(complex);
+
+			//// TODO: 2017/07/04 add default wash types
+			WashType washType = new WashType("Full Wash",90.00);
+			WashType washType1 = new WashType("Interior Wash", 50.00);
+			washTypeService.addWashType(washType);
+			washTypeService.addWashType(washType1);
+
+			//// TODO: 2017/07/17 add default washes
+			WashTime washTime = new WashTime("08:00 to 10:00");
+			WashTime washTime1 = new WashTime("10:00 to 11:00");
+
+			washTimeService.addWashTime(washTime);
+			washTimeService.addWashTime(washTime1);
+
+
+			//// TODO: 2017/06/30 add default user
 			//adding categories and faculties
 			/*User user = new User("Emmie","Madupo",211391901,"emmanuelmadupo@gmail.com","12345",true,role,"0839628990",true);
 			System.out.println("User about to be added is " + user);
 			userService.addUser(user);*/
 		};
 	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileCarWashApplication.class, args);
