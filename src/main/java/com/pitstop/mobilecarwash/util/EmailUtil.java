ea2b1bbb700af84df7ab57d8e99fa0c5f3f260f9
Emmanuel Madupo <emmanuelmadupo@gmail.com>
2017-07-25 21:41:33 +0200

    fixed email issue

--- src/main/java/com/pitstop/mobilecarwash/util/EmailUtil.java
+++ src/main/java/com/pitstop/mobilecarwash/util/EmailUtil.java
@@ -75,7 +75,7 @@ public class EmailUtil {
                         }
                     });
 
-        String emailText= "Email is from @ %s." +"\n" + "Contact number: %s" +"\n\n"+ " Message below:" +"\n" + "%s";
+        String emailText= "Sender Email:  %s." +"\n" + "Sender Contact number: %s" +"\n\n"+ " Message below:" +"\n" + "%s";
         emailText = String.format(emailText,senderId, contactNumber, emailMessage);
         System.out.println("**Formatted sms text: "+emailText);
 
